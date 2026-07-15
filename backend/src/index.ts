02b97c483aa91bc6f36fa75ca6bca19ceebe4967
toantran980 <jokemaking90@gmail.com>
2026-07-14 19:33:41 -0700

    Fix: fix locally fail to load .env file if not in root folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
-import 'dotenv/config';
+//import 'dotenv/config'; //locally fail to load .env files if not in root folder
+import dotenv from 'dotenv';
 import { createApp } from './app';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = createApp();
 const PORT = process.env.PORT || 4000;
+
 
 app.listen(PORT, () => {
   console.log(`text2diagram backend listening on http://localhost:${PORT}`);
